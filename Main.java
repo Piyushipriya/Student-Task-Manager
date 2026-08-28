121e66797828dfb94ca87f287f948b1e16eab349
Piyushipriya <piyushipriya791@gmail.com>
2026-08-28 22:34:37 +0530

    Add task menu and task list

--- Main.java
+++ Main.java
@@ -1,5 +1,52 @@
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("========== STUDENT TASK MANAGER ==========");
+       Scanner sc = new Scanner(System.in);
+        ArrayList<String> tasks = new ArrayList<>();
+
+        while (true) {
+
+            System.out.println("\n===== STUDENT TASK MANAGER =====");
+            System.out.println("1. Add Task");
+            System.out.println("2. View Tasks");
+            System.out.println("3. Exit");
+
+            System.out.print("Enter your choice: ");
+            int choice = sc.nextInt();
+            sc.nextLine();
+
+            if (choice == 1) {
+
+                System.out.print("Enter your task: ");
+                String task = sc.nextLine();
+
+                tasks.add(task);
+
+                System.out.println("Task added successfully!");
+
+            } else if (choice == 2) {
+
+                System.out.println("\nYour Tasks:");
+
+                if (tasks.isEmpty()) {
+                    System.out.println("No tasks available.");
+                } else {
+                    for (int i = 0; i < tasks.size(); i++) {
+                        System.out.println((i + 1) + ". " + tasks.get(i));
+                    }
+                }
+
+            } else if (choice == 3) {
+
+                System.out.println("Thank you for using Student Task Manager!");
+                break;
+
+            } else {
+
+                System.out.println("Invalid choice. Try again.");
+            }
+        }
+
+        sc.close();
     }
 }
